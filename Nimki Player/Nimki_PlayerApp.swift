2976fc6f8dd28c32a6ebf32a58ddc42a819a5222
Sourav Bhattacharjee <sourav.bhattacharjee78@gmail.com>
2025-04-10 01:26:22 +0530

    Initial Commit

---
+++ Nimki Player/Nimki_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nimki_PlayerApp.swift
+//  Nimki Player
+//
+//  Created by Sourav Bhattacharjee on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Nimki_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
